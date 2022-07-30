c135987c195f3b96beb969ba2601c8deba9ba1d9
VIT-NQV <106851524+VIT-NQV@users.noreply.github.com>
2022-07-31 00:42:17 +0700

    31/07 Create

---
+++ WebTaskManager/src/test/java/com/example/webtaskmanager/WebTaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webtaskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTaskManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
